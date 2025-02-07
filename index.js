72877bc183fb4c0f4acf1a11afe3fdb2e74e2be8
cGoyal69 <cgoy96@gmail.com>
2025-02-07 11:28:21 +0530

    change1

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const attendanceCount = (rollNumber) => {
       `SELECT COUNT(*) AS count 
        FROM attendance 
        WHERE rollNo = ? 
-       AND date <= ? `,
+       AND date >= ? `,
       [rollNumber,istTime],
       (err, result) => {
         if (err) {
